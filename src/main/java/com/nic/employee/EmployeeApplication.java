14565445238d6703bcdc70451c3525a521777227
root <nagarajan10102000@gmail.com>
2023-05-08 15:28:13 +0530

    https://github.com/nagarajan-kc/Springboot-Thymeleaf-CRUD.git

--- src/main/java/com/nic/employee/EmployeeApplication.java
+++ src/main/java/com/nic/employee/EmployeeApplication.java
@@ -2,6 +2,8 @@ package com.nic.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
 @SpringBootApplication
 public class EmployeeApplication {
@@ -10,4 +12,14 @@ public class EmployeeApplication {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
+	@Bean
+	public SpringResourceTemplateResolver htmlTemplateResolver()
+	{
+	    SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+	    resolver.setPrefix("classpath:/templates/");
+	    resolver.setSuffix(".html"); // here
+	    resolver.setTemplateMode("HTML5");
+	    resolver.setOrder(1);
+	    return resolver;
+	}
 }
